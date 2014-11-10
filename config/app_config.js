148b460519ef2ef2d80067ebeb099888e00465b6
Amit Shrivastava <amit.shrivastava@fidelis-inc.com>
2014-11-10 12:38:53 +0530

    Corrected path for parent folder.

--- config/app_config.js
+++ config/app_config.js
@@ -22,6 +22,8 @@ module.exports = function Config(app) {
     app.use(bodyParser.urlencoded({extended: false}));
     app.use(cookieParser());
 
+    app.use(require('less-middleware')(path.join(__dirname,'..', 'public')));
+    app.use(express.static(path.join(__dirname,'..', 'public')));
 
     // catch 404 and forward to error handler
     app.use(function (req, res, next) {
